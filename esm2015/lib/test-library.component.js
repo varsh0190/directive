5249ebf387068963c6314bebddfdc12d0f2c94b3
Varsharani Pawar <varsharani.pawar@philips.com>
2018-07-31 16:24:09 +0530

    updated versaion

--- esm2015/lib/test-library.component.js
+++ esm2015/lib/test-library.component.js
@@ -16,8 +16,8 @@ TestLibraryComponent.decorators = [
                 selector: 'lib-test-library',
                 template: `
     <p>
-      *******test-library works Bharath Rocks
-      <b>version 0.0.1</b>******** !
+      *******test-library works 
+      <b>version 0.0.3</b>******** !
     
     </p>
   `,
@@ -27,4 +27,4 @@ TestLibraryComponent.decorators = [
 /** @nocollapse */
 TestLibraryComponent.ctorParameters = () => [];
 
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidGVzdC1saWJyYXJ5LmNvbXBvbmVudC5qcyIsInNvdXJjZVJvb3QiOiJuZzovL3Rlc3QtbGlicmFyeS8iLCJzb3VyY2VzIjpbImxpYi90ZXN0LWxpYnJhcnkuY29tcG9uZW50LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7QUFBQSxPQUFPLEVBQUUsU0FBUyxFQUFVLE1BQU0sZUFBZSxDQUFDO0FBYWxELE1BQU07SUFFSixpQkFBaUI7Ozs7SUFFakIsUUFBUTtLQUNQOzs7WUFoQkYsU0FBUyxTQUFDO2dCQUNULFFBQVEsRUFBRSxrQkFBa0I7Z0JBQzVCLFFBQVEsRUFBRTs7Ozs7O0dBTVQ7Z0JBQ0QsTUFBTSxFQUFFLEVBQUU7YUFDWCIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7IENvbXBvbmVudCwgT25Jbml0IH0gZnJvbSAnQGFuZ3VsYXIvY29yZSc7XG5cbkBDb21wb25lbnQoe1xuICBzZWxlY3RvcjogJ2xpYi10ZXN0LWxpYnJhcnknLFxuICB0ZW1wbGF0ZTogYFxuICAgIDxwPlxuICAgICAgKioqKioqKnRlc3QtbGlicmFyeSB3b3JrcyBCaGFyYXRoIFJvY2tzXG4gICAgICA8Yj52ZXJzaW9uIDAuMC4xPC9iPioqKioqKioqICFcbiAgICBcbiAgICA8L3A+XG4gIGAsXG4gIHN0eWxlczogW11cbn0pXG5leHBvcnQgY2xhc3MgVGVzdExpYnJhcnlDb21wb25lbnQgaW1wbGVtZW50cyBPbkluaXQge1xuXG4gIGNvbnN0cnVjdG9yKCkgeyB9XG5cbiAgbmdPbkluaXQoKSB7XG4gIH1cblxufVxuXG4iXX0=
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidGVzdC1saWJyYXJ5LmNvbXBvbmVudC5qcyIsInNvdXJjZVJvb3QiOiJuZzovL3Rlc3QtbGlicmFyeS8iLCJzb3VyY2VzIjpbImxpYi90ZXN0LWxpYnJhcnkuY29tcG9uZW50LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7QUFBQSxPQUFPLEVBQUUsU0FBUyxFQUFVLE1BQU0sZUFBZSxDQUFDO0FBYWxELE1BQU07SUFFSixpQkFBaUI7Ozs7SUFFakIsUUFBUTtLQUNQOzs7WUFoQkYsU0FBUyxTQUFDO2dCQUNULFFBQVEsRUFBRSxrQkFBa0I7Z0JBQzVCLFFBQVEsRUFBRTs7Ozs7O0dBTVQ7Z0JBQ0QsTUFBTSxFQUFFLEVBQUU7YUFDWCIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7IENvbXBvbmVudCwgT25Jbml0IH0gZnJvbSAnQGFuZ3VsYXIvY29yZSc7XG5cbkBDb21wb25lbnQoe1xuICBzZWxlY3RvcjogJ2xpYi10ZXN0LWxpYnJhcnknLFxuICB0ZW1wbGF0ZTogYFxuICAgIDxwPlxuICAgICAgKioqKioqKnRlc3QtbGlicmFyeSB3b3JrcyBcbiAgICAgIDxiPnZlcnNpb24gMC4wLjM8L2I+KioqKioqKiogIVxuICAgIFxuICAgIDwvcD5cbiAgYCxcbiAgc3R5bGVzOiBbXVxufSlcbmV4cG9ydCBjbGFzcyBUZXN0TGlicmFyeUNvbXBvbmVudCBpbXBsZW1lbnRzIE9uSW5pdCB7XG5cbiAgY29uc3RydWN0b3IoKSB7IH1cblxuICBuZ09uSW5pdCgpIHtcbiAgfVxuXG59XG5cbiJdfQ==
--- esm5/lib/test-library.component.js
+++ esm5/lib/test-library.component.js
@@ -17,7 +17,7 @@ var TestLibraryComponent = /** @class */ (function () {
     TestLibraryComponent.decorators = [
         { type: Component, args: [{
                     selector: 'lib-test-library',
-                    template: "\n    <p>\n      *******test-library works Bharath Rocks\n      <b>version 0.0.1</b>******** !\n    \n    </p>\n  ",
+                    template: "\n    <p>\n      *******test-library works \n      <b>version 0.0.3</b>******** !\n    \n    </p>\n  ",
                     styles: []
                 },] },
     ];
@@ -27,4 +27,4 @@ var TestLibraryComponent = /** @class */ (function () {
 }());
 export { TestLibraryComponent };
 
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidGVzdC1saWJyYXJ5LmNvbXBvbmVudC5qcyIsInNvdXJjZVJvb3QiOiJuZzovL3Rlc3QtbGlicmFyeS8iLCJzb3VyY2VzIjpbImxpYi90ZXN0LWxpYnJhcnkuY29tcG9uZW50LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7QUFBQSxPQUFPLEVBQUUsU0FBUyxFQUFVLE1BQU0sZUFBZSxDQUFDOztJQWVoRDtLQUFpQjs7OztJQUVqQix1Q0FBUTs7O0lBQVI7S0FDQzs7Z0JBaEJGLFNBQVMsU0FBQztvQkFDVCxRQUFRLEVBQUUsa0JBQWtCO29CQUM1QixRQUFRLEVBQUUsb0hBTVQ7b0JBQ0QsTUFBTSxFQUFFLEVBQUU7aUJBQ1g7Ozs7K0JBWkQ7O1NBYWEsb0JBQW9CIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IHsgQ29tcG9uZW50LCBPbkluaXQgfSBmcm9tICdAYW5ndWxhci9jb3JlJztcblxuQENvbXBvbmVudCh7XG4gIHNlbGVjdG9yOiAnbGliLXRlc3QtbGlicmFyeScsXG4gIHRlbXBsYXRlOiBgXG4gICAgPHA+XG4gICAgICAqKioqKioqdGVzdC1saWJyYXJ5IHdvcmtzIEJoYXJhdGggUm9ja3NcbiAgICAgIDxiPnZlcnNpb24gMC4wLjE8L2I+KioqKioqKiogIVxuICAgIFxuICAgIDwvcD5cbiAgYCxcbiAgc3R5bGVzOiBbXVxufSlcbmV4cG9ydCBjbGFzcyBUZXN0TGlicmFyeUNvbXBvbmVudCBpbXBsZW1lbnRzIE9uSW5pdCB7XG5cbiAgY29uc3RydWN0b3IoKSB7IH1cblxuICBuZ09uSW5pdCgpIHtcbiAgfVxuXG59XG5cbiJdfQ==
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidGVzdC1saWJyYXJ5LmNvbXBvbmVudC5qcyIsInNvdXJjZVJvb3QiOiJuZzovL3Rlc3QtbGlicmFyeS8iLCJzb3VyY2VzIjpbImxpYi90ZXN0LWxpYnJhcnkuY29tcG9uZW50LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7QUFBQSxPQUFPLEVBQUUsU0FBUyxFQUFVLE1BQU0sZUFBZSxDQUFDOztJQWVoRDtLQUFpQjs7OztJQUVqQix1Q0FBUTs7O0lBQVI7S0FDQzs7Z0JBaEJGLFNBQVMsU0FBQztvQkFDVCxRQUFRLEVBQUUsa0JBQWtCO29CQUM1QixRQUFRLEVBQUUsdUdBTVQ7b0JBQ0QsTUFBTSxFQUFFLEVBQUU7aUJBQ1g7Ozs7K0JBWkQ7O1NBYWEsb0JBQW9CIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IHsgQ29tcG9uZW50LCBPbkluaXQgfSBmcm9tICdAYW5ndWxhci9jb3JlJztcblxuQENvbXBvbmVudCh7XG4gIHNlbGVjdG9yOiAnbGliLXRlc3QtbGlicmFyeScsXG4gIHRlbXBsYXRlOiBgXG4gICAgPHA+XG4gICAgICAqKioqKioqdGVzdC1saWJyYXJ5IHdvcmtzIFxuICAgICAgPGI+dmVyc2lvbiAwLjAuMzwvYj4qKioqKioqKiAhXG4gICAgXG4gICAgPC9wPlxuICBgLFxuICBzdHlsZXM6IFtdXG59KVxuZXhwb3J0IGNsYXNzIFRlc3RMaWJyYXJ5Q29tcG9uZW50IGltcGxlbWVudHMgT25Jbml0IHtcblxuICBjb25zdHJ1Y3RvcigpIHsgfVxuXG4gIG5nT25Jbml0KCkge1xuICB9XG5cbn1cblxuIl19
